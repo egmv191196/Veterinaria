52e8efb69ccb0610b4dcec14ff93382a468e5634
Carmen Rojas <carmen23.r.spezzia@gmail.com>
2021-04-20 21:13:19 -0500

    interfaz de proveedores cliente y productos

---
+++ js/Mostrar.js
@@ -0,0 +1,10 @@
+function Mostrar() {
+    horaActual=new Date();
+    hora=horaActual.getHours();
+    minuto=horaActual.getMinutes();
+    segundo=horaActual-getSeconds();
+
+    horaimpre= hora + ":" + minuto+":"+ segundo;
+
+    document.getElementById('hora').innerHTML = horaimpre;
+}
---
+++ productos.php
@@ -0,0 +1,158 @@
+<!doctype html>
+<html lang="es" xmlns:th="http://www.w3.org/1999/xhtml">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"/>
+    <script src="https://kit.fontawesome.com/8c9db8153c.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href=".//css/cabezapie.css">
+    <link rel="stylesheet" href=".//css/menu.css">
+    <script type="text/javascript" src="./js/Mostrar.js"></script>
+    <title>Menú-Veterinaria Balbi</title>
+  </head>
+  <body>
+    <div id="Home">
+      <!---- Navigation -->
+        <nav class="navbar navbar-expand-md navbar-black fixed-top">
+          <a class="navbar-brand" href="menu.php"><img src="img/BALBI-sin-fondo.png"></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+            <img src="img/Enca/menu.svg" class="img-fluid">
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <h1>Veterinaria Balbi</h1>
+              </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                  <li class="nav-item">
+                    <div id="hora"><script type="text/javascript">Mostrar();</script></div>
+                  </li>
+                  <li class="nav-item">
+                    <a href="" class="nav-link">
+                      <i class="fa fa-user fa-2x" aria-hidden="true"></i>
+                    </a>
+                </li>
+                  <li class="nav-item">
+                      <a href="" class="nav-link">
+                        <i class="fa fa-sign-out fa-2x" aria-hidden="true"></i>
+                      </a>
+                  </li>
+          </ul>
+          </div>
+        </nav>
+    </div>	
+  <div class="header">
+    <div class="col-12">
+        <h1>Productos <i class="fas fa-box-open"></i></h1>
+        <a href="#VenRegistrar" class="btn btn-success mb-2" data-toggle="modal">Agregar</a>
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                <thead>
+                <tr style="text-align: center">
+                    <th>Código </th>
+                    <th>Nombre</th>
+                    <th>Proveedor</th>
+                    <th>Precio de compra</th>
+                    <th>Precio de venta normal</th>
+                    <th>Precio de venta medico</th>
+                    <th>Precio de venta mayoreo</th>
+                    <th>Existencia</th>
+
+                    <th>Editar</th>
+                    <th>Eliminar</th>
+                </tr>
+                </thead>
+                <tbody>
+                  <?php
+                  require_once('./php/conexion.php');
+                  $result= mysqli_query($conexion, "select * from producto");
+                  while ($row = mysqli_fetch_array($result)) {
+                      echo '<tr style="text-align: center">';
+                      echo '<td>'.$row['id_Producto'].'</td>';
+                      echo '<td>'.$row['Nombre'].'</td>';
+                      echo '<td></td>';
+                      echo '<td>'.$row['p_Compra'].'</td>';
+                      echo '<td>'.$row['p_VentaN'].'</td>';
+                      echo '<td>'.$row['p_VentaMe'].'</td>';
+                      echo '<td>'.$row['p_VentaMa'].'</td>';
+                      echo '<td>'.$row['Cantidad'].'</td>';
+                      echo '<td> <a class="btn btn-warning" href="#"><i class="fa fa-edit"></i></a> </td>';
+                      echo '<td> <form action="#" method="post">';
+                      echo '<button type="submit" class="btn btn-danger"> <i class="fa fa-trash"></i> </button>';
+                      echo '</form></td>';
+                      echo '</tr>';
+                  }
+                  ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="modal fade" id="VenRegistrar">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button tyle="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h2 class="modal-title">Registrar producto</h2>
+              </div>
+              <div class="modal-body">
+              <form method="POST" action="./php/addProducto.php">
+          
+          <div class="form-group">
+              <label class="label">Código </label>
+              <input required autocomplete="off" name="id_Producto" class="form-control"
+                     type="text" placeholder="Código ">
+          </div>
+          <div class="form-group">
+              <label class="label">Nombre</label>
+              <input required autocomplete="off" name="Nombre" class="form-control"
+                     type="text" placeholder="Nombre">
+          </div>
+          <div class="form-group">
+            <label class="label">Proveedor</label>
+            <input required autocomplete="off" name="Proveedor" class="form-control"
+                   type="text" placeholder="Proveedor">
+        </div>
+          <div class="form-group">
+              <label class="label">Precio de compra</label>
+              <input required autocomplete="off" name="p_Compra" class="form-control"
+                     type="decimal(9,2)" placeholder="Precio de compra">
+          </div>
+          <div class="form-group">
+              <label class="label">Precio de venta normal</label>
+              <input required autocomplete="off" name="p_VentaC" class="form-control"
+                     type="decimal(9,2)" placeholder="Precio de venta normal">
+          </div>
+          <div class="form-group">
+            <label class="label">Precio de venta medico</label>
+            <input required autocomplete="off" name="p_VentaM" class="form-control"
+                   type="decimal(9,2)" placeholder="Precio de venta medico">
+        </div>
+        <div class="form-group">
+          <label class="label">Precio de venta mayoreo</label>
+          <input required autocomplete="off" name="p_VentaMa" class="form-control"
+                 type="decimal(9,2)" placeholder="Precio de venta mayoreo">
+        </div>
+          <div class="form-group">
+              <label class="label">Existencia</label>
+              <input required autocomplete="off" name="stock" class="form-control"
+                     type="decimal(9,2)" placeholder="Existencia">
+          </div>
+          <button class="btn btn-success">Guardar</button>
+          <button type="button"class="btn btn-primary" data-dismiss="modal">Cerrar</button>
+      </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+  </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
